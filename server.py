75a835de962eca231da2450556acc32a7707328a
kdevivanco <kdevivanco@gmail.com>
2022-11-25 16:17:00 -0500

    done

--- server.py
+++ server.py
@@ -5,8 +5,26 @@ app.secret_key = 'keep it secret, keep it safe'
 
 @app.route('/')
 def index():
-    pass
+    return render_template('index.html')
 
+
+@app.route('/redirect',methods = ['POST'])
+def direction():
+    session['form_answer'] = request.form
+    print(session['form_answer'])
+    return redirect('/result')
+
+@app.route('/result')
+def show_result():
+    user_input = session['form_answer']
+    print(user_input['name'])
+    print(user_input['city'])
+    print(user_input['fav_lan'])
+    return render_template('result.html',user_input=user_input)
+
+@app.route('/return',methods = ['POST'])
+def return_():
+    return redirect('/')
 
 
 if __name__ == "__main__":
